0ec2c0109224cfd03b639a5339b5ad11e4fc1c8c
Veteran0411 <115995378+Veteran0411@users.noreply.github.com>
2023-03-12 14:24:16 +0530

    added final code

---
+++ ashram.js
@@ -0,0 +1,47 @@
+const navslide= () => {
+    const bars=document.querySelector('.som');
+    const nav = document.querySelector('.nav-links');
+   
+    
+    bars.addEventListener('click',()=>{
+        // toogle effect
+        nav.classList.toggle('nav-active');
+     });
+}
+
+navslide();
+
+// for sliding effect
+var swiper = new Swiper(".team-slid", {
+    loops:true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints:{
+        "0": {
+        slidesPerView: 1,
+        autoplay:{
+            delay:100,
+            disableOnInteraction:false,
+        },
+
+    },
+    "768":{
+        slidesPerView:2,
+    },
+    "1024":{
+        slidesPerView:3,
+    },
+},
+  });
+
+  function display(){
+   let str= document.getElementById("story");
+   str.style.display='block';
+
+}
+
+function hide(){
+    let str= document.getElementById("story");
+    str.style.display='none';
+ 
+ }
